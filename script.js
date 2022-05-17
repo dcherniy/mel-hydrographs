f45e1c58098687339321ccabc6aed7c7131aab06
Daniel C <higheruppa@gmail.com>
2022-05-16 20:16:09 -0400

    Removed tick marks, changed styles

--- script.js
+++ script.js
@@ -211,28 +211,20 @@ const createCtx = (id, key) => {
         maintainAspectRatio: false,
         data: {
             datasets: [{
-                label: null, // label for the chart
                 data: gaugedata[id][key],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
             }]
         },
         options: {
+            elements: {
+                line: {
+                    borderWidth: 2,
+                    borderColor: '#0047AB'
+                },
+                point: {
+                    borderColor: '#0047AB',
+                    radius: 0
+                }
+            },
             plugins: {
                 legend: {
                     display: false
@@ -254,5 +246,4 @@ const createCtx = (id, key) => {
     });
     charts[id]['activeriver'] = river;
     charts[id]['activekey'] = key;
-    console.log(charts[id])
 }
